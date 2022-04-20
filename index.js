37215fbf6cd7f68530a55d1de3d56bb8af8c83a3
John Netzel <johnrnetzel@gmail.com>
2022-04-20 15:49:48 -0800

    Add inquirer prompt for prject title

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 
-console.log(inquirer);
+inquirer.prompt(
+    [
+        {
+            type: "input",
+            name: "title",
+            message: "Please enter the name of your project:"
+        }
+    ]
+).then(response =>
+{
+    console.log(response);
+})
